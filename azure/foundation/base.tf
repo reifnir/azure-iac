f253cf0d6075289b9560e4fbabbbbecedf29db35
Jim <jim@andreasen.dev>
2026-06-11 16:04:24 -0400

    switch backend from http to azurerm

--- azure/foundation/base.tf
+++ azure/foundation/base.tf
@@ -1,7 +1,12 @@
 terraform {
-  backend "http" {
-    # Relies on environment variables for configuration
+  backend "azurerm" {
+    subscription_id       = "8df18e1b-269a-426f-a321-ca437966c787" # Pro MSDN subscription (no variables allowed here)
+    resource_group_name  = "rg-common-storage"
+    storage_account_name = "sareifnircommonstorage"
+    container_name       = "terraform-state"
+    key                  = "iac/terraform.tfstate"
   }
+
   required_providers {
     gitlab = {
       source = "gitlabhq/gitlab"
