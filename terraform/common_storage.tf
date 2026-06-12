aa6d58bea3c66be780c0e332763e69c1364d67e6
Jim <jim@andreasen.dev>
2026-06-12 09:58:35 -0400

    fmt

--- terraform/common_storage.tf
+++ terraform/common_storage.tf
@@ -10,16 +10,16 @@ resource "azurerm_storage_account" "common" {
   resource_group_name = azurerm_resource_group.common_storage.name
   location            = var.location
 
-  account_replication_type = "LRS"
-  account_tier             = "Standard"
+  account_replication_type        = "LRS"
+  account_tier                    = "Standard"
   allow_nested_items_to_be_public = false
-  min_tls_version          = "TLS1_2"
+  min_tls_version                 = "TLS1_2"
 
   tags = local.tags
 }
 
 resource "azurerm_storage_container" "terraform_state" {
-  name                 = "terraform-state"
+  name               = "terraform-state"
   storage_account_id = azurerm_storage_account.common.id
 
   # Yes it's default, but let's be explicit about it. We don't want to accidentally make this public.
